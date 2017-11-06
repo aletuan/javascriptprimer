bae32ce068a6b2b5024b2113e2da97932698402a
Tuan Anh Le <anhlt1983@gmail.com>
2017-11-06 17:43:28 +0900

    extra argument

--- primer.js
+++ primer.js
@@ -1,6 +1,22 @@
 //console.log("Hello");
 // define and using function
-var myFunc = function () {
+/*
+let myFunc = function() {
     console.log("This is a statement");
 };
+
 myFunc();
+*/
+// using rest parameter
+var myFunc = function (name, weather) {
+    var extraArgs = [];
+    for (var _i = 2; _i < arguments.length; _i++) {
+        extraArgs[_i - 2] = arguments[_i];
+    }
+    console.log("Hello " + name + ".");
+    console.log("It is " + weather + " today");
+    for (var i = 0; i < extraArgs.length; i++) {
+        console.log("Extra Args: " + extraArgs[i]);
+    }
+};
+myFunc("Adam", "sunny", "one", "two", "three");
--- primer.ts
+++ primer.ts
@@ -1,7 +1,19 @@
 //console.log("Hello");
 // define and using function
+/*
 let myFunc = function() {
     console.log("This is a statement");
 };
 
 myFunc();
+*/
+// using rest parameter
+let myFunc = function(name, weather, ...extraArgs) {
+    console.log("Hello " + name + ".");
+    console.log("It is " + weather + " today");
+    for (let i = 0; i < extraArgs.length; i++) {
+        console.log("Extra Args: " + extraArgs[i]);
+    }
+}
+
+myFunc("Adam", "sunny", "one", "two", "three");
